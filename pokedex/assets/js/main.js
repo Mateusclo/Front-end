5df0fa2d915907932267b1017510f1529b10e2ca
mateus <matepadij@gmail.com>
2022-11-04 19:31:58 -0300

    js

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,22 +1,39 @@
-const offset = 0;
-const limit = 10;
+const offset = 1;
+const limit = 11;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then((response) => {
+function convertPokemonHtml(pokemon){
+    return`
+        <li>
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
 
-    return response.json();
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">Glass</li>
+                        <li class="type">Poison</li>
+                    </ol>
 
-}).then(function(response){
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                    alt="${pokemon.name}">
+                
+                </div> 
+            </li>
+        `
+}
+const pokemonList = document.getElementById('pokeList')
 
-    console.log(response.results);
+pokeApi.getPokemons().then((pokemon)=> {
 
+    const listItens = []
+
+    for (let i = 0; i < pokemon.length; i++) {
+        const pokemons = pokemon[i];
+        listItens.push(convertPokemonHtml(pokemons))
+        
+    }
+    console.log(listItens)
+
+    pokemonList.innerHTML += convertPokemonHtml(pokemons)
 })
-.catch(function(error){
 
-    console.error(error);
-
-}).finally(function(){
-
-    console.log('Requisição concluída');
-
-});
